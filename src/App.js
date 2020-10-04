253dc3bbbad7365e9ce4807d3171540d08fef8af
Emnamn <emna.manai.7@gmail.com>
2020-10-04 12:08:08 +0100

    changing style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
 
-import { Button, Form, FormGroup, label, Input }
+import { FormGroup, }
   from 'reactstrap';
-import { FacebookLoginButton } from "react-social-login-buttons";
 
 function App() {
-  return (<div>
+  return (<div style={{ textAlign: 'center' }}>
     <form className="login-form">
-      <h1> <span className= "font-weight-bold">LOGIN PAGE</span></h1>
+      <h1 style={{ color: "red", fontSize: 60 }}> <span className="font-weight-bold" >LOGIN PAGE</span></h1>
 
-<h2>Welcome to our Form </h2>
-    <input className="form-control"/>
-    <FormGroup> 
-      <label>Email</label>
-      <input type="email" placeholder="Email"/>
-        </FormGroup>
-        <FormGroup> 
-      <label>Password</label>
-      <input type="password" placeholder="Password"/>
-        </FormGroup>
-        <button>log in</button>
+      <h2>Welcome to our Form </h2>
+      <input className="form-control" />
+      <FormGroup>
+        <label>Email</label>
+        <input type="email" placeholder="Email" />
+      </FormGroup>
+      <FormGroup>
+        <label>Password</label>
+        <input type="password" placeholder="Password" />
+      </FormGroup>
+      <button>log in</button>
     </form>
 
 
